4993525610579c5ff7cdeb31ddf25a0805eed6bd
Hugues Collet <hc.pro@outlook.fr>
2023-08-24 12:40:38 +0200

    Change home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,18 @@
+import ReactLogo from "../assets/react.svg";
+import viteLogo from "../../public/vite.svg";
+
 function Home() {
   return (
     <div className="padding home">
       <h1>Page Accueil du site</h1>
-      <p>
+      <p className="home-para">
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet nobis
         neque reprehenderit enim eum nulla, esse rerum repudiandae quo obcaecati
         porro nam, ab quaerat. Placeat praesentium voluptatum alias debitis
         animi, ex expedita sapiente eveniet! Quia?
       </p>
-      <h2>Vidéo d'exemple</h2>
+      <img src={ReactLogo} alt="React" width={150} />
+      <img src={viteLogo} alt="React" width={120} />
     </div>
   );
 }
